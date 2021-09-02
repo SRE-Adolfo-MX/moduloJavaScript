1a2d73d2c9df0dee7406a8f23dd2b44e41957aa4
SRE-Adolfo-MX <57839502+SRE-Adolfo-MX@users.noreply.github.com>
2021-09-01 19:33:13 -0500

    Add files via upload

---
+++ 02-sesion-basic/js/main.js
@@ -0,0 +1,10 @@
+console.error("Esto es un error")
+
+console.warn("Esto es una alerta")
+
+// 3 metodos para interactuar con el usuario 
+
+//alert("Esto es una alerta")
+
+prompt("Ingresa tu nombre")
+// esto regresa un string
