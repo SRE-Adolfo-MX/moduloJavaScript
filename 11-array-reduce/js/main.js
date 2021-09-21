534165437b74ae318b26e8460ac044de082b05cb
SRE-Adolfo.MX <adolfoing15@hotmail.com>
2021-09-20 22:03:50 -0500

    Ejercicio clase 11

---
+++ 11-array-reduce/js/main.js
@@ -0,0 +1,31 @@
+let numbersArrays = [20,15,2,5];
+
+let suma = numbersArrays.reduce((accum, number)=>{
+    return accum += number;
+},0)
+
+console.log(suma)
+
+const arrayFruits = [
+    "banana",
+    "fresa",
+    "chicozapote", 
+    "naranja", 
+    "banana",
+    "pera",
+    "pera",
+    "naranja"
+]
+// spray operator
+const copyArrayFruit = {...arrayFruits, licuado: "banana"}
+
+console.log(copyArrayFruit);
+
+const result = arrayFruits.reduce((accum, fruit, index)=>{
+    let count = accum[fruit] ? accum[fruit] + 1 : 1
+    console.log(count)
+    return {
+        ...accum,
+        [fruit]: count
+    }
+},{})
