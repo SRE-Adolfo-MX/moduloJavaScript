e14634c3ebdffefda18516b5fa9b6d6d769c2f7e
SRE-Adolfo.MX <adolfoing15@hotmail.com>
2021-09-21 20:50:50 -0500

    Ejercicio clase 12

---
+++ 12-array-reduce/js/main.js
@@ -0,0 +1,80 @@
+
+const songsData = [
+    {
+        name: "¿Dónde jugarán los niños?",
+        band: "Maná",
+        releaseYear: "1992",
+        statistics: {
+            likes: 20000,
+            reproductions: 8000
+        },
+    },
+    {
+        name: "La muralla verde",
+        band: "Enanitos Verdes",
+        releaseYear: "1986",
+        statistics: {
+            likes: 21000,
+            reproductions: 19000
+        },
+    },
+    {
+        name: "Te Ví En Un Tren",
+        band: "Enanitos Verdes",
+        releaseYear: "1987",
+        statistics: {
+            likes: 20000,
+            reproductions: 23490
+        },
+    },
+    {
+        name: "Mariposa Traicionera",
+        band: "Maná",
+        releaseYear: "2002",
+        statistics: {
+            likes: 12000,
+            reproductions: 25690
+        },
+    },
+    {
+        name: "Rayando el Sol",
+        band: "Maná",
+        releaseYear: "1990",
+        statistics: {
+            likes: 12000,
+            reproductions: 18000
+        },
+    },
+    {
+        name: "La celula que explota",
+        band: "Jaguares",
+        releaseYear: "1999",
+        statistics: {
+            likes: 12000,
+            reproductions: 23421
+        },
+    },
+    {
+        name: "No dejes que...",
+        band: "Jaguares",
+        releaseYear: "1999",
+        statistics: {
+            likes: 12345,
+            reproductions: 10000
+        },
+    }
+]
+
+const getSongsByBand = arraySongs => {
+    let songs = songsData.reduce((accum, song) =>{
+        const { band } = song;
+        return !accum[band]
+        ? {...accum, [band]: [song.name]} // No existe la propiedad
+        : {...accum, [band]: [...accum[band], song.name]} // cuando ya existe
+    },{})
+    console.log(songs)
+}
+
+getSongsByBand(songsData);
+
+
